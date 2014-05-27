2af05f4cebaacb0c25fa9a426b5141488fa8c8c1
Jason Rojas <jason.rojas@mgo.com>
2014-05-27 10:57:13 -0700

    initial commit

---
+++ captions.js
@@ -0,0 +1,9 @@
+var async = require('async'),
+    srt = require('./lib/srt.js'),
+    smi = require('./lib/smi.js'),
+    scc = require('./lib/scc.js'),
+    vtt = require('./lib/vtt.js'),
+    xml = require('./lib/xml.js'),
+    ttml = require('./lib/ttml.js');
+
+
