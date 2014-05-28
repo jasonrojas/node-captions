250a38898b962f96f6cbea2cfb4b535a8b7c69ee
jasonrojas <jason@nothingbeatsaduck.com>
2014-05-28 12:36:43 -0700

    Update captions.js

--- captions.js
+++ captions.js
@@ -2,7 +2,7 @@
 * Creates an instance of the node-captions module. This module allows you to convert from
 * one caption format to another. See the README.
 * @module node-captions
-* @version 0.9
+* @version 0.0.1
 * @author Jason Rojas <jason.rojas@mgo.com>
 * @see http://github.com/jasonrojas/node-captions
 * @returns An instance of the node-captions module.
