16a24348a57d95e79e9f60908b3f9ef6844aeacd
Julien Rottenberg <Julien.Rottenberg@mgo.com>
2014-12-05 10:29:17 -0800

    adding lcov support to close the task

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,8 +22,7 @@ module.exports = function (grunt) {
                         functions: 84,
                         lines: 83
                     },
-                    reportFormats: ['text', 'html'],
-                    //reportFormats: ['text', 'html', 'cobertura'],
+                    reportFormats: ['text', 'html', 'lcov'],
                     coverageFolder: 'build/report'
                 }
             }
@@ -63,11 +62,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-mocha-istanbul');
     grunt.loadNpmTasks('grunt-jsdoc');
     grunt.registerTask('default', ['jshint', 'jslint', 'mocha_istanbul:coverage', 'jsdoc']);
-
     grunt.event.on('coverage', function (lcov, done) {
         if (lcov) {
             done();
         }
     });
-
 };
