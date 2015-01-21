f884a1a61be647bbdf61053f959e3866fad880ef
Ufuk Altinok <ufukaltinok@gmail.com>
2015-01-21 17:35:16 +0200

    Added simple MicroDVD/Sub format parser

--- captions.js
+++ captions.js
@@ -15,6 +15,7 @@ module.exports = {
     scc: require('./lib/scc.js'),
     srt: require('./lib/srt.js'),
     smi: require('./lib/smi.js'),
+    sub: require('./lib/sub.js'),
     vtt: require('./lib/vtt.js'),
     smpte_tt: require('./lib/smpte_tt.js'),
     ttml: require('./lib/ttml.js'),
