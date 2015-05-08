a78962ec64eb778ee06ab2b178ecd92cda3df8ee
Jason Rojas <jason.rojas@mgo.com>
2015-05-08 13:22:29 -0700

    removing jsonlint in favor of beautifier

--- captions.js
+++ captions.js
@@ -1,12 +1,12 @@
 /**
-* Creates an instance of the node-captions module. This module allows you to convert from
-* one caption format to another. See the README.
-* @module node-captions
-* @version 0.0.1
-* @author Jason Rojas <jason@nothingbeatsaduck.com>
-* @see http://github.com/jasonrojas/node-captions
-* @returns An instance of the node-captions module.
-*/
+ * Creates an instance of the node-captions module. This module allows you to convert from
+ * one caption format to another. See the README.
+ * @module node-captions
+ * @version 0.0.1
+ * @author Jason Rojas <jason@nothingbeatsaduck.com>
+ * @see http://github.com/jasonrojas/node-captions
+ * @returns An instance of the node-captions module.
+ */
 
 var iconv = require('iconv-lite');
 iconv.extendNodeEncodings();
